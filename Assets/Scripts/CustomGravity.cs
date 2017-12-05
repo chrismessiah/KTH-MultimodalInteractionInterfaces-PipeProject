b4131d01ea4787a0f6a044b090f33c46fa4d85f5
Christian Abdelmassih <chrabd@kth.se>
2017-12-05 11:02:11 +0100

    Add custom gravity script

---
+++ Assets/Scripts/CustomGravity.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomGravity : MonoBehaviour {
+
+	public float speed = 800.0f;
+	Vector3 movementVector = new Vector3 (0.0f, 0.0f, -1.0f);
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate () {
+		GetComponent<Rigidbody> ().AddForce (movementVector * speed * Time.deltaTime);
+	}
+}
