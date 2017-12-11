5ece2588f444ed411fce07d097f6a73c2f434322
Christian Abdelmassih <chrabd@kth.se>
2017-12-11 14:30:14 +0100

    Add more models and stuff

--- Assets/Scripts/SpeedPowerUp.cs
+++ Assets/Scripts/SpeedPowerUp.cs
@@ -7,8 +7,8 @@ public class SpeedPowerUp : MonoBehaviour {
 	float speed = 20000.0f;
 	string E1Str = "Entrence_1", E2Str = "Entrence_2"; // need to be unique
 
-	GameObject E1, E2;
-	Rigidbody Ball;
+	GameObject E1, E2, Ball;
+	Rigidbody Ball_rb;
 	Vector3 E1toE2, E2toE1, movementVector;
 
 	static bool triggerE1 = false, triggerE2 = false;
@@ -17,7 +17,8 @@ public class SpeedPowerUp : MonoBehaviour {
 	void Start () {
 		E1 = GameObject.Find(E1Str);
 		E2 = GameObject.Find(E2Str);
-		Ball = GameObject.Find("Ball").GetComponent<Rigidbody> ();
+		Ball = GameObject.Find("Ball");
+		Ball_rb = Ball.GetComponent<Rigidbody> ();
 
 		// Create vector E1->E2 (assuming entrence is E1)
 		E1toE2 = E2.transform.position - E1.transform.position;
@@ -48,13 +49,14 @@ public class SpeedPowerUp : MonoBehaviour {
 				triggerE1 = false;
 				triggerE2 = false;
 				//movementVector = null;
+				Ball_rb.velocity = Ball_rb.velocity.normalized * 10;
 			}
 		}
 	}
 
 	void FixedUpdate () {
 		if (triggerE1 || triggerE2) {
-			Ball.AddForce (movementVector * speed * Time.deltaTime);
+			Ball_rb.AddForce (movementVector * speed * Time.deltaTime);
 		}
 	}
 
