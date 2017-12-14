8a72f44629efdf0f405dfd9d476cddf85aebdeac
Christian Abdelmassih <chrabd@kth.se>
2017-12-14 12:05:23 +0100

    Add button to spawn balls

---
+++ Assets/Scripts/ButtonClick.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonClick : MonoBehaviour {
+
+	public Button yourButton;
+	public Transform child;
+	public Transform parent;
+
+	void Start()
+	{
+		Button btn = yourButton.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick()
+	{
+		Instantiate(child, new Vector3(0, 0, -40), Quaternion.identity, parent);
+	}
+}
